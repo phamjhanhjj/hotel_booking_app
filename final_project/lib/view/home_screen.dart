fef86940c5aef3b20a1a3680e8e92ef2096fd50f
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2024-09-30 16:41:42 +0700

    navigation

--- final_project/lib/view/home_screen.dart
+++ final_project/lib/view/home_screen.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      backgroundColor: Colors.blue,
-      // bottomNavigationBar: const CustomNavBar(index: 0),
-      body: SafeArea(
-        bottom: false,
-        child: Stack(
-          children: [
-            Container(
-              height: double.infinity,
-              margin: EdgeInsets.only(top: size.height * 0.25),
-              color: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: size.height * 0.25),
+        color: Colors.white,
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // _HeaderSection(),
+                // _SearchCard(),
+                // SizedBox(height: 20),
+                // _NearbyHotels(),
+              ],
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    // _HeaderSection(),
-                    // _SearchCard(),
-                    // SizedBox(height: 20),
-                    // _NearbyHotels(),
-                  ],
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Maps',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_online),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 0, 242, 255),
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
